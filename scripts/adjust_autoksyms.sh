303f3aae31ceb0f7232fd4b0ad518d6171d1a213
Quentin Perret <qperret@google.com>
2020-02-18 09:41:37 +0000

    BACKPORT: FROMLIST: kbuild: allow symbol whitelisting with TRIM_UNUSED_KSYMS
    CONFIG_TRIM_UNUSED_KSYMS currently removes all unused exported symbols
    from ksymtab. This works really well when using in-tree drivers, but
    cannot be used in its current form if some of them are out-of-tree.
    
    Indeed, even if the list of symbols required by out-of-tree drivers is
    known at compile time, the only solution today to guarantee these don't
    get trimmed is to set CONFIG_TRIM_UNUSED_KSYMS=n. This not only wastes
    space, but also makes it difficult to control the ABI usable by vendor
    modules in distribution kernels such as Android. Being able to control
    the kernel ABI surface is particularly useful to ship a unique Generic
    Kernel Image (GKI) for all vendors, which is a first step in the
    direction of getting all vendors to contribute their code upstream.
    
    As such, attempt to improve the situation by enabling users to specify a
    symbol 'whitelist' at compile time. Any symbol specified in this
    whitelist will be kept exported when CONFIG_TRIM_UNUSED_KSYMS is set,
    even if it has no in-tree user. The whitelist is defined as a simple
    text file, listing symbols, one per line.
    
    [ BACKPORT: 60ae1b194b45 not present, sed 3rd line of modules.order ]
    
    Bug: 148277666
    Acked-by: Jessica Yu <jeyu@kernel.org>
    Acked-by: Nicolas Pitre <nico@fluxnic.net>
    Tested-by: Matthias Maennich <maennich@google.com>
    Reviewed-by: Matthias Maennich <maennich@google.com>
    Signed-off-by: Quentin Perret <qperret@google.com>
    Link: https://lore.kernel.org/lkml/20200218094139.78835-2-qperret@google.com
    Change-Id: Ie9a4bbb8eebb20202623f084859fb89e55a32698

--- scripts/adjust_autoksyms.sh
+++ scripts/adjust_autoksyms.sh
@@ -40,8 +40,16 @@ esac
 # We need access to CONFIG_ symbols
 . include/config/auto.conf
 
-# In case it doesn't exist yet...
-if [ -e "$cur_ksyms_file" ]; then touch "$cur_ksyms_file"; fi
+ksym_wl=/dev/null
+if [ -n "$CONFIG_UNUSED_KSYMS_WHITELIST" ]; then
+	# Use 'eval' to expand the whitelist path and check if it is relative
+	eval ksym_wl="$CONFIG_UNUSED_KSYMS_WHITELIST"
+	[ "${ksym_wl}" != "${ksym_wl#/}" ] || ksym_wl="$abs_srctree/$ksym_wl"
+	if [ ! -f "$ksym_wl" ]; then
+		echo "ERROR: '$ksym_wl' whitelist file not found" >&2
+		exit 1
+	fi
+fi
 
 # Generate a new ksym list file with symbols needed by the current
 # set of modules.
@@ -53,6 +61,7 @@ cat > "$new_ksyms_file" << EOT
 EOT
 sed 's/ko$/mod/' modules.order |
 xargs -n1 sed -n -e '3{s/ /\n/g;/^$/!p;}' -- |
+cat - "$ksym_wl" |
 sort -u |
 sed -e 's/\(.*\)/#define __KSYM_\1 1/' >> "$new_ksyms_file"
 
