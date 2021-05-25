5011880b47168acc61bb31034d46fd807134887a
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-05-25 16:32:53 +0530

    ARM64/configs: ginkgo: Enable fuse short circuit
    Signed-off-by: fajar <santuyz321@gmail.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1379,13 +1379,10 @@ struct task_struct {
 	void				*security;
 #endif
 
-<<<<<<< HEAD
-=======
 #ifdef CONFIG_FUSE_SHORTCIRCUIT
 	int fuse_boost;
 #endif
 
->>>>>>> efee1432663b (fs: fuse: Implement fuse short circuit)
 	/*
 	 * New fields for task_struct should be added above here, so that
 	 * they are included in the randomized portion of task_struct.
