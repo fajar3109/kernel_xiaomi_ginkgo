84b4d50f5d586fcad84469c4dffef90777511288
fajar <santuyz321@gmail.com>
2022-02-02 12:06:42 +0000

    treewide: fix failed build
    Signed-off-by: fajar <santuyz321@gmail.com>

--- include/net/rtnetlink.h
+++ include/net/rtnetlink.h
@@ -93,7 +93,8 @@ struct rtnl_link_ops {
 	unsigned int		(*get_num_tx_queues)(void);
 	unsigned int		(*get_num_rx_queues)(void);
 
-	unsigned int		slave_maxtype;
+	bool			netns_refund;
+	int			slave_maxtype;
 	const struct nla_policy	*slave_policy;
 	int			(*slave_validate)(struct nlattr *tb[],
 						  struct nlattr *data[],
