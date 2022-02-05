d57cb838f6e43c9f8b225692b98e60debb1d7b37
fajar <santuyz321@gmail.com>
2022-02-05 22:49:19 +0700

    b.sh: regen
    Signed-off-by: fajar <santuyz321@gmail.com>

--- b.sh
+++ b.sh
@@ -1,3 +1,9 @@
+sudo ln -sf /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
+bold=$(tput bold)
+normal=$(tput sgr0)
+
+echo -e "${bold}Compile Sedang Berlangsung...${normal}"
+
 SECONDS=0 # builtin bash timer
 ZIPNAME="GabutKernel-4.14.264-CAF-R-ginkgo-$(date '+%Y%m%d-%H%M').zip"
 SDC_DIR="/workspace/Gitpod-Workspaces/sdc-clang"
@@ -9,9 +15,9 @@ DEFCONFIG="vendor/ginkgo-perf_defconfig"
 export PATH="${SDC_DIR}/compiler/bin:${GCC64_DIR}/bin:${GCC_DIR}/bin:/usr/bin:${PATH}"
 
 if ! [ -d "$SDC_DIR" ]; then
-echo "SnapDragon clang not found! Cloning to $SDC_DIR..."
+echo "Clang Tidak Ditemukan! Clone ke $SDC_DIR..."
 if ! git clone -b master --depth=1 https://github.com/ThankYouMario/proprietary_vendor_qcom_sdclang -b 14 $SDC_DIR; then
-echo "Cloning failed! Aborting..."
+echo "CLone Gagal! Batalkan..."
 exit 1
 fi
 fi
@@ -52,11 +58,11 @@ echo -e "\nStarting compilation...\n"
 make -j$(nproc --all) O=out ARCH=arm64 LD_LIBRARY_PATH="${SDC_DIR}/lib:${LD_LIBRARY_PATH}" CC=clang LD=ld.lld AR=llvm-ar AS=llvm-as NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip CROSS_COMPILE=aarch64-linux-android- CROSS_COMPILE_ARM32=arm-linux-androideabi- CLANG_TRIPLE=aarch64-linux-gnu- Image.gz-dtb dtbo.img
 
 if [ -f "out/arch/arm64/boot/Image.gz-dtb" ] && [ -f "out/arch/arm64/boot/dtbo.img" ]; then
-echo -e "\nKernel compiled succesfully! Zipping up...\n"
+echo -e "\nKernel Sukses Di Compile! Zipping up...\n"
 if [ -d "$AK3_DIR" ]; then
 cp -r $AK3_DIR AnyKernel3
 elif ! git clone -q -b gabut https://github.com/fajar3109/AnyKernel3; then
-echo -e "\nAnyKernel3 repo not found locally and cloning failed! Aborting..."
+echo -e "${bold}Direktori AnyKernel3 Sudah Ada, Tidak Perlu di Clone${normal}"
 exit 1
 fi
 cp out/arch/arm64/boot/Image.gz-dtb AnyKernel3
@@ -68,12 +74,12 @@ zip -r9 "../$ZIPNAME" * -x '*.git*' README.md *placeholder
 cd ..
 rm -rf AnyKernel3
 rm -rf out/arch/arm64/boot
-echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
+echo -e "\n Berhasil! dalam waktu $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
 echo "Zip: $ZIPNAME"
 if ! [[ $HOSTNAME = "gitpod" && $USER = "fajar" ]]; then
 curl -T $ZIPNAME temp.sh; echo
 fi
 else
-echo -e "\nCompilation failed!"
+echo -e "\nCompile Gagal! Tolong Perbaiki Masalah Ini"
 exit 1
 fi
